b762143d11bd047b9ce3758a0c7b35b5a9535fde
Dragan Trojanovic <dragan.t@orange.fr>
2023-02-16 19:53:12 +0100

    ajout du menu non responsive

--- wp-content/themes/twentytwenty-child/footer.php
+++ wp-content/themes/twentytwenty-child/footer.php
@@ -1,5 +1,5 @@
 <footer id="site-footer" class="header-footer-group">
-             <a href="#">Mentions légales</a>
+              <a class="footer-custom" href="#">Mentions légales</a>
         </footer><!-- #site-footer -->
 
       <?php wp_footer(); ?>

ef797cc05d799e3b6f8c0f33ab29d12f40e9ee56
Dragan Trojanovic <dragan.t@orange.fr>
2023-02-27 19:18:40 +0100

    Ajout du menu admin et responsive ok

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -6,5 +6,11 @@ register_nav_menus( array(
 	'footer' => 'Bas de page',
 ) );
 
-
+function add_admin_link_to_menu( $items, $args ) {
+    if ( is_user_logged_in() && $args->theme_location == 'main' ) {
+        $items .= '<li><a class="admin" href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2 );
 
